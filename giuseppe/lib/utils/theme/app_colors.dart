d38dbc579fd9a96ad0098d0160241dba5980ded0
Francisco-Suntaxi07 <stalynfran@hotmail.com>
2024-11-17 19:07:41 -0500

    Creacion del tema y colores de la app

---
+++ giuseppe/lib/utils/theme/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Colores de la app
+  static const Color primaryColor = Color.fromRGBO(255, 255, 255, 1.0);
+  static const Color primaryVariantColor = Color.fromRGBO(164, 164, 164, 0.75);
+  static const Color secondaryColor = Color.fromRGBO(0, 0, 0, 1.0);
+  static const Color secondaryVariantColor = Color.fromRGBO(66, 65, 65, 1.0);
+  static const Color backgroundColor = Color.fromRGBO(255, 255, 255, 1.0);
+  static const Color surfaceColor= Color.fromRGBO(255, 255, 255, 1.0);
+  static const Color errorColor = Color.fromRGBO(255, 0, 0, 1.0);
+  static const Color onPrimaryColor = Color.fromRGBO(0, 0, 0, 1.0);
+  static const Color onSecondaryColor = Color.fromRGBO(40, 39, 39, 1.0);
+  static const Color onBackgroundColor = Color.fromRGBO(255, 255, 255, 1.0);
+  static const Color onSurfaceColor = Color.fromRGBO(0, 0, 0, 1.0);
+  static const Color onErrorColor = Color.fromRGBO(25, 0, 255, 1.0);
+
+  // Colores de los Textos
+  static const Color primaryTextColor = Color.fromRGBO(0, 0, 0, 1.0);
+  static const Color secondaryTextColor = Color.fromRGBO(255, 255, 255, 1.0);
+  static const Color hintTextColor = Color.fromRGBO(180, 180, 180, 1.0);
+
+}
---
+++ giuseppe/lib/utils/theme/app_theme.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:giuseppe/utils/theme/app_colors.dart';
+
+class AppTheme {
+
+  static ThemeData get generalTheme{
+    return ThemeData(
+      //colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+      primaryColor: AppColors.primaryColor,
+      /*colorScheme: ColorScheme(
+          brightness: brightness,
+          primary: AppColors.primaryColor,
+          onPrimary: onPrimary,
+          secondary: secondary,
+          onSecondary: onSecondary,
+          error: error, onError: onError,
+          surface: surface,
+          onSurface: onSurface
+      ),*/
+      scaffoldBackgroundColor: AppColors.backgroundColor,
+      textTheme: const TextTheme(
+        displaySmall: TextStyle(
+            color: AppColors.primaryTextColor, fontWeight: FontWeight.bold),
+        headlineSmall: TextStyle( // *Para titulos principales
+            color: AppColors.primaryTextColor, fontWeight: FontWeight.bold,fontSize: 30.0),
+        titleLarge: TextStyle(  // *Para subtitulos
+            color: AppColors.primaryTextColor, fontWeight: FontWeight.bold),
+        titleSmall: TextStyle( // *Para texto de botones
+            color: AppColors.primaryTextColor,fontSize: 17.0,  fontWeight: FontWeight.bold ),
+        bodyMedium: TextStyle( // *Para texto de botones
+            color: AppColors.primaryTextColor),
+        bodySmall: TextStyle( // *Para descripciones de las cards
+            color: AppColors.primaryTextColor,fontWeight: FontWeight.w500,fontSize: 13.0),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(AppColors.secondaryColor),
+            foregroundColor: WidgetStateProperty.all(AppColors.secondaryTextColor),
+            shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)
+                )),
+            splashFactory: InkRipple.splashFactory,
+            overlayColor: WidgetStateProperty.all(AppColors.primaryVariantColor),
+          )
+      ),
+    );
+  }
+
+}
