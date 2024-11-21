680475e6fe7861c914a483f474491c3afcf1cc31
Luis Xavier Espinosa Meza <lxespinosa@espe.edu.ec>
2024-11-20 20:10:55 -0500

    Lista de imagenes add product

--- giuseppe/lib/presentation/tabs/inventory/object_form/object_form.dart
+++ giuseppe/lib/presentation/tabs/inventory/object_form/object_form.dart
@@ -45,7 +45,7 @@ class _ObjectFormState extends State<ObjectForm> {
               ),
               Container(
                 padding: const EdgeInsets.symmetric(vertical: 15.0),
-                child: Text("Añadir a inventario",
+                child: Text("AÑADIR A INVENTARIO",
                     style: Theme.of(context).textTheme.headlineSmall
                 )
               ),
@@ -67,27 +67,35 @@ class _NewObjectForm extends StatefulWidget {
 
 class _NewObjectFormState extends State<_NewObjectForm> {
   final ImagePicker picker = ImagePicker();
-  File? _image;
+  final List<File> _images = []; // Lista para almacenar imágenes seleccionadas
 
-  Future<void> pickImageFromGallery() async {
-    final pickFile = await picker.pickImage(source: ImageSource.gallery);
-    if (pickFile != null) {
+  Future<void> pickImagesFromGallery() async {
+    final List<XFile>? pickedFiles = await picker.pickMultiImage(); // Selección múltiple.
+    if (pickedFiles != null) {
       setState(() {
-        _image = File(pickFile.path);
+        _images.addAll(pickedFiles.map((file) => File(file.path)));
       });
     }
+  }
+
+  //Eliminar imagen
+  void removeImage(int index) {
+    setState(() {
+      _images.removeAt(index); //
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     String? selectedCategory;
     List<String> categories = ['Plasticos', 'Metales', 'Vidrio', 'Papel', 'Otros'];
+
     return Card(
       elevation: 0,
       color: Colors.transparent,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
-        side: const BorderSide(color: AppColors.primaryVariantColor, width: 1.0), // Borde gris
+        side: const BorderSide(color: AppColors.primaryVariantColor, width: 1.0),
       ),
       child: Padding(
         padding: const EdgeInsets.all(15.0),
@@ -99,16 +107,58 @@ class _NewObjectFormState extends State<_NewObjectForm> {
                 children: [
                   Column(
                     children: [
-                      Center(
-                          child: _image != null
-                              ? Image.file(_image!, height: 150, fit: BoxFit.fill)
-                              : const Icon(Icons.image,size: 150)
+                      // Carrusel de imágenes
+                      _images.isNotEmpty
+                          ? SizedBox(
+                        height: 150,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: _images.length,
+                          itemBuilder: (context, index) {
+                            return Stack(
+                              children: [
+                                Container(
+                                  margin: const EdgeInsets.all(8.0),
+                                  child: Image.file(
+                                    _images[index],
+                                    height: 150,
+                                    width: 150,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 5,
+                                  right: 5,
+                                  child: GestureDetector(
+                                    onTap: () => removeImage(index),
+                                    //boton para borrar imagenes
+                                    child: Container(
+                                      width: 20,
+                                      height: 20,
+                                      decoration: BoxDecoration(
+                                        color: Colors.black,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: const Icon(
+                                        Icons.close,
+                                        color: Colors.white,
+                                        size: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                      )
+                          : const Center(
+                        child: Icon(Icons.image, size: 150),
                       ),
                       ElevatedButton(
-                        onPressed: (){
-                          pickImageFromGallery();
-                        },
-                        child: const Text("Añadir Imagen"))
+                        onPressed: pickImagesFromGallery,
+                        child: const Text("Añadir Imágenes"),
+                      ),
                     ],
                   ),
                   const SizedBox(height: 15.0),
@@ -173,17 +223,16 @@ class _NewObjectFormState extends State<_NewObjectForm> {
                   const SizedBox(height: 30.0),
                   Center(
                     child: ElevatedButton(
-                      onPressed: (){},
-                      child: const Text("Añadir")
+                      onPressed: () {},
+                      child: const Text("Añadir"),
                     ),
-                  )
+                  ),
                 ],
               ),
             ],
-          )
+          ),
         ),
       ),
     );
   }
 }
-
