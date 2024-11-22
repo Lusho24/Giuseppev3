29bad2b0c30b3848da5165d0958f5c2b96c1a39b
Luis Xavier Espinosa Meza <lxespinosa@espe.edu.ec>
2024-11-22 11:55:36 -0500

    correccion de desbodamientos orden despacho

--- giuseppe/lib/presentation/tabs/dispatch_order/dispatch_order_tab.dart
+++ giuseppe/lib/presentation/tabs/dispatch_order/dispatch_order_tab.dart
@@ -11,11 +11,7 @@ class DispatchOrderTab extends StatefulWidget {
 class _DispatchOrderTabState extends State<DispatchOrderTab> {
   // Simulamos una lista de ordenes
   final List<Map<String, String>> _orders = [
-    {
-      "name": "Nombrssssssssssssssssssssssssssssssssssssssssssssssse",
-      "abiable": "999",
-      'image': 'assets/images/lampara.png'
-    },
+    {"name": "Nombre", "abiable": "999", 'image': 'assets/images/lampara.png'},
     {"name": "Objeto 1", "abiable": "99", 'image': 'assets/images/mesa.png'},
     {"name": "Objeto 2", "abiable": "99", 'image': 'assets/images/mesa.png'},
     {"name": "Silla", "abiable": "20", 'image': 'assets/images/silla.png'},
@@ -45,48 +41,78 @@ class _DispatchOrderTabState extends State<DispatchOrderTab> {
               ),
             ),
           ),
-          // Contenido desplazable
+          // Parte desplazable
           Expanded(
-            child: CustomScrollView(
-              slivers: [
-                SliverGrid(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 1,
-                    mainAxisSpacing: 10.0,
-                    childAspectRatio: 2.1,
-                  ),
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      return Container(
-                        padding: const EdgeInsets.all(5.0),
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            bottom: BorderSide(color: Colors.grey, width: 1.5),
-                          ),
+            child: Column(
+              children: [
+                Expanded(
+                  child: CustomScrollView(
+                    slivers: [
+                      SliverGrid(
+                        gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 1,
+                          childAspectRatio: 1.95,
                         ),
-                        child: OrderCard(order: _orders[index]),
-                      );
-                    },
-                    childCount: _orders.length,
-                  ),
-                ),
-                SliverList(
-                  delegate: SliverChildListDelegate([
-                    Container(
-                      padding: const EdgeInsets.only(
-                          top: 50.0, left: 120.0, right: 120.0, bottom: 20.0),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Siguiente"),
-                            Icon(Icons.arrow_forward),
-                          ],
+                        delegate: SliverChildBuilderDelegate(
+                              (context, index) {
+                            return Container(
+                              padding: const EdgeInsets.all(5.0),
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                      color: Colors.grey, width: 1.5),
+                                ),
+                              ),
+                              child: OrderCard(order: _orders[index]),
+                            );
+                          },
+                          childCount: _orders.length,
                         ),
                       ),
-                    ),
-                  ]),
+                      SliverList(
+                        delegate: SliverChildListDelegate([
+                          Container(
+                            padding: const EdgeInsets.only(
+                              top: 30.0,
+                              left: 100.0,
+                              right: 100.0,
+                              bottom: 20.0,
+                            ),
+                            child: SizedBox(
+                              height: 40,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  padding: EdgeInsets.zero,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(6.0),
+                                  ),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      "Siguiente",
+                                      style: TextStyle(fontSize: 16.0),
+                                    ),
+                                    const SizedBox(width: 20),
+                                    Container(
+                                      child: const Icon(
+                                        Icons.arrow_forward,
+                                        size: 18,
+                                        color: AppColors.primaryColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -107,20 +133,23 @@ class OrderCard extends StatelessWidget {
       elevation: 0,
       color: Colors.white,
       child: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(5.0),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset(
-              order['image']!,
+            SizedBox(
               height: 100.0,
               width: 100.0,
-              fit: BoxFit.cover,
+              child: Image.asset(
+                order['image']!,
+                fit: BoxFit.scaleDown,
+              ),
             ),
             const SizedBox(width: 5.0),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
                 children: [
                   Text(
                     order['name']!,
@@ -129,7 +158,7 @@ class OrderCard extends StatelessWidget {
                       fontSize: 20,
                       fontWeight: FontWeight.w300,
                     ),
-                    overflow: TextOverflow.ellipsis, // Evitar desbordamiento
+                    overflow: TextOverflow.ellipsis,
                   ),
                   Text(
                     "Disponible: ${order['abiable']!}",
@@ -140,7 +169,7 @@ class OrderCard extends StatelessWidget {
                     ),
                     overflow: TextOverflow.ellipsis,
                   ),
-                  const SizedBox(height: 10.0),
+                  const SizedBox(height: 5.0),
                   Row(
                     children: [
                       const Expanded(
@@ -149,18 +178,34 @@ class OrderCard extends StatelessWidget {
                       ElevatedButton(
                         onPressed: () {},
                         style: ElevatedButton.styleFrom(
-                          minimumSize: Size(50, 30),
-                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                          textStyle: TextStyle(fontSize: 14),
+                          minimumSize: const Size(50, 30),
+                          padding:
+                          const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          textStyle: const TextStyle(fontSize: 14),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(6),
                           ),
                         ),
                         child: const Text("Eliminar"),
                       ),
-
                     ],
-                  )
+                  ),
+                  const SizedBox(height: 5.0),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Observaciones:",
+                      hintStyle: const TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.grey,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                    ),
+                  ),
                 ],
               ),
             )
@@ -209,41 +254,39 @@ class _NumberInputState extends State<NumberInput> {
             borderRadius: BorderRadius.circular(6.0),
           ),
           child: IconButton(
-            icon: Icon(Icons.remove),
+            icon: const Icon(Icons.remove),
             iconSize: 15,
             color: AppColors.primaryColor,
-            padding: EdgeInsets.all(0),
             onPressed: _decrement,
           ),
         ),
+        const SizedBox(width: 10.0),
+
         SizedBox(
-          width: 10.0,
-        ),
-        Expanded(
-          child: Container(
-            height: 30.0,
-            child: TextField(
-              enabled: true,
-              controller: _controller,
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16.0),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: '',
-                contentPadding: EdgeInsets.symmetric(vertical: 10.0),
-              ),
-              onChanged: (text) {
-                setState(() {
-                  _currentValue = int.tryParse(text) ?? 0;
-                });
-              },
+          height: 30.0,
+          width: 30.0,
+          child: TextField(
+            enabled: true,
+            controller: _controller,
+            keyboardType: TextInputType.number,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 14.0),
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: '',
+              contentPadding: EdgeInsets.symmetric(vertical: 5.0),
             ),
+            onChanged: (text) {
+              setState(() {
+                _currentValue = int.tryParse(text) ?? 0;
+              });
+            },
           ),
         ),
-        SizedBox(
-          width: 10.0,
-        ),
+
+
+        const SizedBox(width: 10.0),
+
         Container(
           height: 30,
           width: 30,
@@ -253,17 +296,15 @@ class _NumberInputState extends State<NumberInput> {
             borderRadius: BorderRadius.circular(6.0),
           ),
           child: IconButton(
-            icon: Icon(Icons.add),
+            icon: const Icon(Icons.add),
             iconSize: 15,
             color: AppColors.primaryColor,
-            padding: EdgeInsets.all(0),
             onPressed: _increment,
           ),
         ),
-        SizedBox(
-          width: 10.0,
-        ),
+        const SizedBox(width: 10.0),
       ],
     );
   }
+
 }
