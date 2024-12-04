6d2046887cbde0babd95b07d0a0e4fa35c0f6ca9
Luis Xavier Espinosa Meza <lxespinosa@espe.edu.ec>
2024-12-04 18:12:27 -0500

    cambios pagina edit

--- giuseppe/lib/presentation/tabs/inventory/object_form/edit_object_form.dart
+++ giuseppe/lib/presentation/tabs/inventory/object_form/edit_object_form.dart
@@ -58,16 +58,20 @@ class _EditObjectFormState extends State<EditObjectForm> {
               ),
             ],
           ),
+
+
           // Fondo opaco y animación de carga
           if (_isLoading)
             Positioned.fill(
               child: Container(
+                padding: const EdgeInsets.only(top: 40),
                 color: Colors.black.withOpacity(0.75),
                 child: Center(
                   child: Lottie.asset(
                     'assets/lottiefiles/loading1.json',
                     width: 250,
                     height: 250,
+                    repeat: true,
                   ),
                 ),
               ),
@@ -76,6 +80,12 @@ class _EditObjectFormState extends State<EditObjectForm> {
       ),
     );
   }
+  void setLoading(bool isLoading) {
+    setState(() {
+      _isLoading = isLoading;
+    });
+  }
+
 }
 
 class _EditObjectFormBody extends StatefulWidget {
@@ -96,8 +106,8 @@ class _EditObjectFormBody extends StatefulWidget {
 
 class _EditObjectFormBodyState extends State<_EditObjectFormBody> {
   final _formKey = GlobalKey<FormState>();
-  final ImagePicker _picker = ImagePicker();
-
+  final ImagePicker picker = ImagePicker();
+  List<File> _itemImg = []; //imagenes
   late TextEditingController _nameController;
   late TextEditingController _quantityController;
   late TextEditingController _detailController;
@@ -121,27 +131,37 @@ class _EditObjectFormBodyState extends State<_EditObjectFormBody> {
     _selectedCategory = widget.item['category'];
   }
 
-  final List<String> _categories = [
-    'Electrónica',
-    'Ropa',
-    'Muebles',
-    'Deportes',
-    'Libros',
-  ]; // Lista de categorías disponibles
+  List<String> _categories = [
+    'Accesorios', //ia
+    'Auxiliares',
+    'Bases', //ia
+    'Candelabros', //ia
+    'Electrodomésticos',
+    'Herramientas',
+    'Lamparas', //ia
+    'Mobiliario', //ia
+    'Vajilla', //ia
+    'Otros',
+  ];
 
 
-  Future<void> _pickImage() async {
-    if (_localImages.length + _remoteImages.length >= 4) {
+  Future getImages() async {
+    // Limitar a 4 imágenes
+    if (_itemImg.length >= 4) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Máximo 4 imágenes.')),
       );
       return;
     }
-
-    final List<XFile>? selectedImages = await _picker.pickMultiImage();
-    if (selectedImages != null) {
+    // Seleccionar las imágenes
+    final List<XFile>? tempImgs = await picker.pickMultiImage();
+    if (tempImgs != null) {
       setState(() {
-        _localImages.addAll(selectedImages.map((img) => File(img.path)));
+        for (var img in tempImgs) {
+          if (_itemImg.length < 4) {
+            _itemImg.add(File(img.path));
+          }
+        }
       });
     }
   }
@@ -161,158 +181,228 @@ class _EditObjectFormBodyState extends State<_EditObjectFormBody> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: const EdgeInsets.all(20.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            // Carrusel de imágenes
-            Center(
-              child: (_remoteImages.isEmpty && _localImages.isEmpty)
-                  ? const Text("Seleccione Imágenes")
-                  : CarouselSlider(
-                options: CarouselOptions(
-                  height: 120.0,
-                  enlargeCenterPage: true,
-                  autoPlay: false,
-                  enableInfiniteScroll: false,
-                  aspectRatio: 1.0,
-                  viewportFraction: 0.4,
+      child: Card(
+        elevation: 0,
+        color: Colors.transparent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          side: const BorderSide(
+            color: Colors.blueGrey, // Cambia al color deseado
+            width: 1.0,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Carrusel de imágenes
+                Center(
+                  child: (_remoteImages.isEmpty && _localImages.isEmpty)
+                      ? const Text("Seleccione Imágenes")
+                      : CarouselSlider(
+                    options: CarouselOptions(
+                      height: 120.0,
+                      enlargeCenterPage: true,
+                      autoPlay: false,
+                      enableInfiniteScroll: false,
+                      aspectRatio: 1.0,
+                      viewportFraction: 0.4,
+                    ),
+                    items: [
+                      ..._remoteImages.asMap().entries.map((entry) {
+                        int index = entry.key;
+                        String url = entry.value;
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Stack(
+                              children: [
+                                Image.network(
+                                  url,
+                                  height: 120,
+                                  width: 120,
+                                  fit: BoxFit.contain,
+                                  errorBuilder:
+                                      (context, error, stackTrace) {
+                                    return const Icon(
+                                      Icons.error,
+                                      size: 120,
+                                      color: Colors.red,
+                                    );
+                                  },
+                                ),
+                                Positioned(
+                                  right: 0,
+                                  child: GestureDetector(
+                                    onTap: () =>
+                                        _removeImage(index, true),
+                                    child: Container(
+                                      width: 20,
+                                      height: 20,
+                                      decoration: BoxDecoration(
+                                        color: Colors.black,
+                                        borderRadius:
+                                        BorderRadius.circular(5),
+                                      ),
+                                      child: const Icon(
+                                        Icons.close,
+                                        color: Colors.white,
+                                        size: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }).toList(),
+                      ..._localImages.asMap().entries.map((entry) {
+                        int index = entry.key;
+                        File file = entry.value;
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Stack(
+                              children: [
+                                Image.file(
+                                  file,
+                                  height: 120,
+                                  width: 120,
+                                  fit: BoxFit.cover,
+                                ),
+                                Positioned(
+                                  right: 0,
+                                  child: GestureDetector(
+                                    onTap: () =>
+                                        _removeImage(index, false),
+                                    child: Container(
+                                      width: 20,
+                                      height: 20,
+                                      decoration: BoxDecoration(
+                                        color: Colors.black,
+                                        borderRadius:
+                                        BorderRadius.circular(5),
+                                      ),
+                                      child: const Icon(
+                                        Icons.close,
+                                        color: Colors.white,
+                                        size: 20,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }).toList(),
+                    ],
+                  ),
                 ),
-                items: [
-                  ..._remoteImages.asMap().entries.map((entry) {
-                    int index = entry.key;
-                    String url = entry.value;
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Stack(
-                          children: [
-                            Image.network(
-                              url,
-                              height: 120,
-                              width: 120,
-                              fit: BoxFit.cover,
-                              errorBuilder: (context, error, stackTrace) {
-                                return const Icon(Icons.error, size: 120, color: Colors.red);
-                              },
-                            ),
-                            Positioned(
-                              right: 0,
-                              child: GestureDetector(
-                                onTap: () => _removeImage(index, true),
-                                child: Container(
-                                  width: 20,
-                                  height: 20,
-                                  decoration: BoxDecoration(
-                                    color: Colors.black,
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                  child: const Icon(
-                                    Icons.close,
-                                    color: Colors.white,
-                                    size: 20,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      },
+                const SizedBox(height: 15.0),
+                Center(
+                  child: SizedBox(
+                    width: 160,
+                    child: ElevatedButton(
+                      onPressed: getImages,
+                      child: const Text("Añadir Imágenes"),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15.0),
+                // Campos de texto
+                Text('Nombre', style: Theme.of(context).textTheme.bodyMedium),
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    hintText: 'Ingrese nombre del item',
+                  ),
+                  validator: (value) =>
+                  value?.isEmpty == true ? 'Campo requerido' : null,
+                ),
+                const SizedBox(height: 15.0),
+                Text('Cantidad', style: Theme.of(context).textTheme.bodyMedium),
+                TextFormField(
+                  controller: _quantityController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    hintText: 'Ingrese la cantidad en stock',
+                  ),
+                  validator: (value) =>
+                  value?.isEmpty == true ? 'Campo requerido' : null,
+                ),
+                const SizedBox(height: 15.0),
+                Text('Detalle', style: Theme.of(context).textTheme.bodyMedium),
+                TextFormField(
+                  controller: _detailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    hintText: 'Ingrese detalles del item',
+                  ),
+                ),
+                const SizedBox(height: 15.0),
+                Text('Categoría', style: Theme.of(context).textTheme.bodyMedium),
+                const SizedBox(height: 6.0),
+                DropdownButtonFormField<String>(
+                  value: _selectedCategory,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: const BorderSide(
+                        color: Colors.blueGrey,
+                        width: 1.0,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: const BorderSide(
+                        color: Colors.blueGrey,
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                  hint: const Text('Seleccione una categoría'),
+                  items: _categories.map((category) {
+                    return DropdownMenuItem(
+                      value: category,
+                      child: Text(category),
                     );
                   }).toList(),
-                  ..._localImages.asMap().entries.map((entry) {
-                    int index = entry.key;
-                    File file = entry.value;
-                    return Builder(
-                      builder: (BuildContext context) {
-                        return Stack(
-                          children: [
-                            Image.file(
-                              file,
-                              height: 120,
-                              width: 120,
-                              fit: BoxFit.cover,
-                            ),
-                            Positioned(
-                              right: 0,
-                              child: GestureDetector(
-                                onTap: () => _removeImage(index, false),
-                                child: Container(
-                                  width: 20,
-                                  height: 20,
-                                  decoration: BoxDecoration(
-                                    color: Colors.black,
-                                    borderRadius: BorderRadius.circular(5),
-                                  ),
-                                  child: const Icon(
-                                    Icons.close,
-                                    color: Colors.white,
-                                    size: 20,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  },
+                  validator: (value) =>
+                  value == null ? 'Seleccione una categoría' : null,
+                ),
+                const SizedBox(height: 30.0),
+                Center(
+                  child: SizedBox(
+                    width: 160,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState?.validate() == true) {
+                          // Lógica para guardar
+                        }
                       },
-                    );
-                  }).toList(),
-                ],
-              ),
+                      child: const Text("Guardar Cambios"),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(
-              width: 160,
-              child: ElevatedButton(
-                onPressed: _pickImage,
-                child: const Text("Añadir Imágenes"),
-              ),
-            ),
-            const SizedBox(height: 15.0),
-            // Campos de texto
-            TextFormField(
-              controller: _nameController,
-              decoration: const InputDecoration(labelText: 'Nombre'),
-              validator: (value) => value?.isEmpty == true ? 'Campo requerido' : null,
-            ),
-            const SizedBox(height: 15.0),
-            TextFormField(
-              controller: _quantityController,
-              decoration: const InputDecoration(labelText: 'Cantidad'),
-              validator: (value) => value?.isEmpty == true ? 'Campo requerido' : null,
-            ),
-            const SizedBox(height: 15.0),
-            TextFormField(
-              controller: _detailController,
-              decoration: const InputDecoration(labelText: 'Detalle'),
-            ),
-            const SizedBox(height: 15.0),
-            // Campo de categoría
-            DropdownButtonFormField<String>(
-              value: _selectedCategory,
-              items: _categories.map((category) {
-                return DropdownMenuItem(
-                  value: category,
-                  child: Text(category),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  _selectedCategory = value;
-                });
-              },
-              decoration: const InputDecoration(labelText: 'Categoría'),
-              validator: (value) =>
-              value == null || value.isEmpty ? 'Seleccione una categoría' : null,
-            ),
-            const SizedBox(height: 30.0),
-            ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState?.validate() == true) {
-                  // Guardar cambios
-                }
-              },
-              child: const Text("Guardar Cambios"),
-            ),
-          ],
+          ),
         ),
       ),
     );
